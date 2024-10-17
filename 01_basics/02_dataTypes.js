9e5ea6a31319310efdaf35217f08acf088aa7b2a
Ritiksharma2709 <ritiksharmaiit079@gmail.com>
2024-10-17 06:15:51 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code into newer version
+//alert(3+3)//we are using node js not browser
+console.log(3+
+    3);//code readibilty should be high
+    console.log("ritik");
+    let name ="hitesh"
+    let age=18
+    let isLoggedIn=false
+    let state=null
+    //number =>2 to powewr 53
+    //nigint
+    //string=>""
+    //boolean=>true/false;
+    //null=>standalonevalue
+    //undefined=>
+        //symbol=>unique
+    //object
+    console.log(typeof "ritik");
+    console.log(typeof null);
+    //type of null is object
+    console.log(typeof undefined);///undefined
+    
+    
+
