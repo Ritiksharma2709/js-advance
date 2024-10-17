ff8d169a0d07d87773487c63258f884b9f41908c
Ritiksharma2709 <ritiksharmaiit079@gmail.com>
2024-10-17 06:01:53 +0000

    variable files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=14453
+let accountEmail="ritik#google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+//accountId=2//not allowed
+accountEmail="rit@google.com"
+accountPassword="1212212"
+accountCity="delhi"
+console.log(accountId);
+/*
+prefere not to use var beacsuse of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
