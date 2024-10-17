52d46b80046c5e86fd6dd8fb4616854802c24a99
Ritiksharma2709 <ritiksharmaiit079@gmail.com>
2024-10-17 12:28:11 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,24 @@
+//primitive
+//7types : String,Number,Boolean,null,undefined,Symbol,BigInt
+const score=100
+const scorevalue=100.3
+const isLoggedIn=false
+const outsidetemp=null
+let userEmail;
+const id=Symbol('123')
+const anotherId=Symbol('123')//datattype symbol
+console.log(id===anotherId)
+const bigNumber=33344444554455444n
+//refrence(Nun primitive)
+
+//array,Objects,Funtions
+const heroes=["shaktiman","nagraj","doga"]//datatypes object
+let myObj={
+    name:"ritik",
+    age:"22"
+
+}//datatype object
+const myfunction=function(){
+console.log("hello world")
+}//datatype function also known as object function
+console.log(typeof id);
