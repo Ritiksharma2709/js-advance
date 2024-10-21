bb7505d8d115c0b754b6477895ef023ec4eaf329
Ritiksharma2709 <ritiksharmaiit079@gmail.com>
2024-10-21 14:35:52 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month="march"
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "fab":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+//if not break execute all after match except default
+    default:
+        console.log("default case match")
+        break;
+}
