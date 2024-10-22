a27dadcbe4d54a03bc667f4e5fde3b058a6721db
Ritiksharma2709 <ritiksharmaiit079@gmail.com>
2024-10-22 08:15:57 +0000

    filter

---
+++ 05_iteration/six.js
@@ -0,0 +1,16 @@
+const coding=["js","ruby","java","python","cpp"]
+
+// const values=coding.forEach((item)=>{
+// console.log(item)
+// return item//undefined for each does not return any value
+// })
+// console.log(values);//undefined
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+
+// let newNums=myNums.filter((num)=>num>4)
+// let newNums=myNums.filter((num)=>{
+//    return  num>4 //[]
+// })
+
+// 
+//books example
