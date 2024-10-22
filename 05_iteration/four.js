2c891ec6a17845e4a43d5bd7f5bd87a0cc3e942e
Ritiksharma2709 <ritiksharmaiit079@gmail.com>
2024-10-22 07:53:47 +0000

    loops

---
+++ 05_iteration/four.js
@@ -0,0 +1,24 @@
+const myObject={
+js:'javascript',
+cpp:'c++',
+rb:"ruby",
+swift:"swift by apple"
+    
+}
+for (const key in myObject) {
+// console.log(myObject[key])
+// console.log(`${key} shortcut is for ${myObject[key]}`);
+} 
+const programming=["js","rb","py","java","cpp"]
+for (const key in programming) {
+    // console.log(key)
+    // console.log(programming[key]);
+   
+}
+// const map=new Map()//unique values
+// map.set('IN',"india")
+// map.set('USA',"United State of America")
+// map.set('FR',"France")
+// for (const key in map) {
+//     console.log(key);
+// }/map is not for forin
