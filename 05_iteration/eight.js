36fd60d7a5213aba085bff14725d91676cbd0408
Ritiksharma2709 <ritiksharmaiit079@gmail.com>
2024-10-22 09:02:52 +0000

    reduce and map

---
+++ 05_iteration/eight.js
@@ -0,0 +1,29 @@
+//reduce
+const myNums=[1,2,3]
+
+// const myTotal=myNums.reduce( function (acc,curr){
+//     console.log(`acc:${acc} and currval: ${curr}`)
+//     return acc+curr
+// },0)
+const myTotal=myNums.reduce((acc,curr)=>acc+curr,0)
+console.log(myTotal)
+
+
+
+const shoppingCart=[
+    {
+        itemName:"js course",
+        price:999
+    },
+    {
+        itemName:"mobile dev course",
+        price:5999
+    },
+    {
+        itemName:"data science course",
+        price:12999
+    }
+]
+
+const priceToPay=shoppingCart.reduce((acc,item)=>acc+item.price,0)
+console.log(priceToPay);
